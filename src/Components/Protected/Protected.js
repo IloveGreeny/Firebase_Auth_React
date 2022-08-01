1a0369e7b6eb8d8cb6779909b8d2efa677bb8608
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-08-01 21:33:03 +0400

    Update Protected.js

--- src/Components/Protected/Protected.js
+++ src/Components/Protected/Protected.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { UserAuth } from '../../Context/AuthContext';
+import  UserAuth  from '../../Context/AuthContext';
 
 export  default  function Protected  ({ children })  {
   const { user } = UserAuth();
