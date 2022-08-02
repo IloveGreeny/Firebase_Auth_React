935a2b6317fdb275290f399fbdd3a65d2f18cbca
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-08-02 20:50:36 +0400

    Update firebase.js

--- src/FirebaseConfig/firebase.js
+++ src/FirebaseConfig/firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAvpmrszHCyA3rndA49pF7lkX4rgouipL8",
-    authDomain: "login-app-696c9.firebaseapp.com",
-    projectId: "login-app-696c9",
-    storageBucket: "login-app-696c9.appspot.com",
-    messagingSenderId: "667312531189",
-    appId: "1:667312531189:web:dfc173cb3769df4e9c47bc"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+your firebase config
